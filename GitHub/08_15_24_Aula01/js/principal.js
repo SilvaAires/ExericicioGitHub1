50ac4a7d930153580d7ed76f9aad6b2e1285642a
Thiago Aires <thiago.2021319130@aluno.iffar.edu.br>
2024-09-26 19:38:32 -0300

    javaScript mudado no banner

--- GitHub/08_15_24_Aula01/js/principal.js
+++ GitHub/08_15_24_Aula01/js/principal.js
@@ -1,4 +1,4 @@
-var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!", "Do lixo ao luxo!", "reaproveitar é aproveitar!"];
 var banner = 0;
 console.log("Hello world!");
 alert("Hello! I am an alert box!!");
